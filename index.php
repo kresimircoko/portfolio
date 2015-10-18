ab018e65b3ca93fe51a1725e755657c5329aa6e0
Krešimir Čoko <kresimircoko@gmail.com>
2015-10-18 18:00:17 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Krešimir Čoko</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <header class="site-header">
+      <div class="container">
+        <h1>Krešimir Čoko</h1>
+      </div>
+    </header>
+
+    <?php
+
+      include 'sections/typography.php';
+      include 'sections/structure.php';
+      include 'sections/colors.php';
+      include 'sections/elements.php';
+
+    ?>
+  </body>
+</html>
