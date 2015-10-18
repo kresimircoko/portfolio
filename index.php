6157ab731cbf54633080e217739611b440854ea0
Krešimir Čoko <kresimircoko@gmail.com>
2015-10-18 21:59:55 +0200

    Updated _structure

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Krešimir Čoko</title>
-    <link rel="stylesheet" href="css/style.css">
-  </head>
-  <body>
-    <header class="site-header">
-      <div class="container">
-        <h1>Krešimir Čoko</h1>
-      </div>
-    </header>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Krešimir Čoko</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<header class="site-header">
+			<div class="container">
+				<h1>Krešimir Čoko</h1>
+			</div>
+		</header>
 
-    <?php
+		<?php
 
-      include 'sections/typography.php';
-      include 'sections/structure.php';
-      include 'sections/colors.php';
-      include 'sections/elements.php';
+			include 'sections/hero.php';
+			include 'sections/about.php';
+			include 'sections/achievements.php';
+			include 'sections/interests.php';
+			include 'sections/work.php';
+			include 'sections/contact.php';
 
-    ?>
-  </body>
+		?>
+	</body>
 </html>
