a46f0892ac588590c72096b6a33a5ea31990c863
Krešimir Čoko <kresimircoko@gmail.com>
2015-10-26 16:18:25 +0100

    link fix

--- sections/work.php
+++ sections/work.php
@@ -7,7 +7,7 @@
 			</h1>
 		</div>
 
-		<a href="http://apartmanisandra.com">
+		<a href="http://apartmanisandra.com" target="_blank">
 			<div id="sandra-work" class="full-section subsection">
 				<div class="half-section">
 					<h2 class="subsection-heading">Apartments Sandra</h2>
@@ -15,7 +15,7 @@
 			</div>
 		</a>
 
-		<a href="http://scsi.alwaysdata.net">
+		<a href="http://scsi.alwaysdata.net" target="_blank">
 			<div id="scsi-work" class="full-section subsection">
 				<div class="half-section">
 					<h2 class="subsection-heading">Student Center Šibenik</h2>
@@ -23,7 +23,7 @@
 			</div>
 		</a>
 
-		<a href="http://neobit.hol.es">
+		<a href="http://neobit.hol.es" target="_blank">
 			<div id="neobit-work" class="full-section subsection">
 				<div class="half-section">
 					<h2 class="subsection-heading">Neobit Design</h2>
