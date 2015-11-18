76ab93159abda232027e26c8f3c51092394fb9a4
Krešimir Čoko <kresimircoko@gmail.com>
2015-11-18 16:37:51 +0100

    Tiny fixes

--- sections/blog.php
+++ sections/blog.php
@@ -2,7 +2,7 @@
 
   <div href="#" class="post">
     <div class="container">
-  		<a class="post-content half-section subsection">
+  		<a href="https://medium.com/@kresimircoko/making-a-portfolio-cc2590aee5ad" class="post-content half-section subsection">
   			<h1 class="post-title">Building a portfolio</h1>
         <span class="post-date">18/11/2015</span>
   			<p class="post-description">
